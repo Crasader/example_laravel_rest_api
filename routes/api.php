9cf028cf3651d14335f069b7ba61f59bdd46fa93
Anton <anton.kozyr.dev@gmail.com>
2018-04-23 14:38:05 +0300

    * Remove unnecessary files

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('/', function () {
+    echo 'hello world from api';
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
